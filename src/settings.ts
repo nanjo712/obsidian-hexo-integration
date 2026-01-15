cf915319c7e489cb3c5461e3cfaced31eeecd157
woshiren <yejiacong@outlook.com>
2026-01-15 14:00:38 +0800

    implement 'publish post'

--- src/settings.ts
+++ src/settings.ts
@@ -1,36 +1,49 @@
-import {App, PluginSettingTab, Setting} from "obsidian";
+import { App, PluginSettingTab, Setting } from "obsidian";
 import HexoIntegration from "./main";
 
 export interface HexoIntegrationSettings {
-	mySetting: string;
+    mySetting: string;
+    hexoRoot: string;
 }
 
 export const DEFAULT_SETTINGS: HexoIntegrationSettings = {
-	mySetting: 'default'
+    mySetting: 'default',
+    hexoRoot: ''
 }
 
 export class HexoIntegrationSettingTab extends PluginSettingTab {
-	plugin: HexoIntegration;
+    plugin: HexoIntegration;
 
-	constructor(app: App, plugin: HexoIntegration) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    constructor(app: App, plugin: HexoIntegration) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	display(): void {
-		const {containerEl} = this;
+    display(): void {
+        const { containerEl } = this;
 
-		containerEl.empty();
+        containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Settings #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        new Setting(containerEl)
+            .setName('Settings #1')
+            .setDesc('It\'s a secret')
+            .addText(text => text
+                .setPlaceholder('Enter your secret')
+                .setValue(this.plugin.settings.mySetting)
+                .onChange(async (value) => {
+                    this.plugin.settings.mySetting = value;
+                    await this.plugin.saveSettings();
+                }));
+
+        new Setting(containerEl)
+            .setName('Hexo Root Directory')
+            .setDesc('The absolute path to your Hexo blog root directory.')
+            .addText(text => text
+                .setPlaceholder('/path/to/your/hexo/blog')
+                .setValue(this.plugin.settings.hexoRoot)
+                .onChange(async (value) => {
+                    this.plugin.settings.hexoRoot = value;
+                    await this.plugin.saveSettings();
+                }));
+    }
 }

310987536ad6953990e61751dea67547dbad9d79
woshiren <yejiacong@outlook.com>
2026-01-16 12:48:00 +0800

    change the logic of converting hexo format

--- src/services/PostService.ts
+++ src/services/PostService.ts
@@ -21,14 +21,13 @@ export class PostService {
         private filenameService: FilenameService
     ) { }
 
-    private getFormattedDate(): string {
-        const now = new Date();
-        const year = now.getFullYear();
-        const month = String(now.getMonth() + 1).padStart(2, '0');
-        const day = String(now.getDate()).padStart(2, '0');
-        const hours = String(now.getHours()).padStart(2, '0');
-        const minutes = String(now.getMinutes()).padStart(2, '0');
-        const seconds = String(now.getSeconds()).padStart(2, '0');
+    private getFormattedDate(date: Date = new Date()): string {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        const hours = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+        const seconds = String(date.getSeconds()).padStart(2, '0');
         return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
     }
 
@@ -72,12 +71,13 @@ export class PostService {
     }
 
     async convertToHexo(file: TFile) {
+        const creationTime = new Date(file.stat.ctime);
         const generatedPermalink = await this.permalinkService.generatePermalink(file.basename);
         await this.app.fileManager.processFrontMatter(file, (frontmatter: Record<string, unknown>) => {
             if (!frontmatter.title) frontmatter.title = file.basename;
-            if (!frontmatter.permalink) frontmatter.permalink = generatedPermalink;
-            if (!frontmatter.date) frontmatter.date = this.getFormattedDate();
+            if (!frontmatter.date) frontmatter.date = this.getFormattedDate(creationTime);
             if (!frontmatter.tags) frontmatter.tags = [];
+            if (this.settings.permalinkStyle != 'manual' && !frontmatter.permalink) frontmatter.permalink = generatedPermalink;
             if (frontmatter.published === undefined) frontmatter.published = false;
         });
         new Notice(t('NOTICE_CONVERT_SUCCESS', { fileName: file.name }));
